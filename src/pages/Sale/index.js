f34fee1f078d0bae37bf23f91bf9285ff61bad6a
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2020-04-18 16:15:39 -0300

    Adjustment in index page

--- src/pages/Sale/index.js
+++ src/pages/Sale/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 
 const Sale = () => {
+  const [newcus, setNewcus] = useState(false);
   return (
     <>
       <Header />
@@ -28,12 +29,14 @@ const Sale = () => {
       </div>
 
       <section style={{ marginTop: 10 }}>
+        {}
         <div className="container">
           <div className="field">
             <div className="control">
               <div className="select is-primary">
                 <select name="customer" id="customer" defaultValue={"DEFAULT"}>
                   <option value="DEFAULT">Cliente</option>
+                  <option value="DEFAULT">Luisa</option>
                 </select>
               </div>
             </div>
@@ -42,39 +45,50 @@ const Sale = () => {
           <div className="field">
             <div className="control">
               <label htmlFor="new-client" className="checkbox">
-                <input type="checkbox" name="new-client" id="new-client" />
+                <input
+                  type="checkbox"
+                  name="new-client"
+                  id="new-client"
+                  onChange={() => setNewcus(!newcus)}
+                />
                 <span> Novo cliente?</span>
               </label>
             </div>
           </div>
 
-          <div className="columns">
-            <div className="column is-half is-three-fifths-mobile is-offset-one-fifth-mobile">
-              <div className="field">
-                <div className="control">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Nome completo"
-                  />
+          {newcus && (
+            <div className="columns">
+              <div className="column is-half is-three-fifths-mobile is-offset-one-fifth-mobile">
+                <div className="field">
+                  <div className="control">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Nome completo"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input type="email" className="input" placeholder="E-mail" />
+                <div className="field">
+                  <div className="control">
+                    <input
+                      type="email"
+                      className="input"
+                      placeholder="E-mail"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="field is-grouped is-grouped-centered">
-                <p className="control">
-                  <button className="button is-link">Salvar</button>
-                </p>
-                <p className="control">
-                  <button className="button">Cancelar</button>
-                </p>
+                <div className="field is-grouped is-grouped-centered">
+                  <p className="control">
+                    <button className="button is-link">Salvar</button>
+                  </p>
+                  <p className="control">
+                    <button className="button">Cancelar</button>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </section>
     </>
