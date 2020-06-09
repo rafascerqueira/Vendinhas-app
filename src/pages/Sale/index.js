6342fb3aab823f1ffacc817f97d5e6f9ae504e60
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2020-06-08 22:05:47 -0300

    new reload when set a new Customer

--- src/pages/Sale/index.js
+++ src/pages/Sale/index.js
@@ -109,7 +109,10 @@ const Sale = () => {
                     <button
                       className="button is-link"
                       type="button"
-                      onClick={() => setNewCustomer({ fullname, email })}
+                      onClick={() => {
+                        setNewCustomer({ fullname, email });
+                        window.location.reload();
+                      }}
                     >
                       Salvar
                     </button>
@@ -122,27 +125,88 @@ const Sale = () => {
             </div>
           )}
           {!newcus && (
-            <div className="field">
-              <label className="label">Lançar produtos</label>
-              <div className="field is-horizontal has-addons">
-                <p className="control">
-                  <label className="label">cód.:</label>
-                  <input className="input" type="text" maxLength="6" size="4" />
-                </p>
-                <p className="control">
-                  <label className="label">Nome</label>
-                  <input className="input" type="text" readOnly />
-                </p>
-                <p className="control">
-                  <label className="label">Qtde.</label>
-                  <input className="input" type="text" maxLength="2" size="2" />
-                </p>
-                <p className="control">
-                  <label className="label">Incluir</label>
-                  <Link to="#" className="button is-info">
-                    +
-                  </Link>
-                </p>
+            <div className="columns">
+              <div className="column is-11-mobile is-offset-1-mobile">
+                <div className="field">
+                  <label className="label">Lançar produtos</label>
+                  <div className="field is-horizontal has-addons">
+                    <p className="control">
+                      <label className="label">cód.:</label>
+                      <input
+                        className="input"
+                        type="text"
+                        maxLength="6"
+                        size="4"
+                      />
+                    </p>
+                    <p className="control">
+                      <label className="label">Nome</label>
+                      <input className="input" type="text" readOnly />
+                    </p>
+                    <p className="control">
+                      <label className="label">Qtde.</label>
+                      <input
+                        className="input"
+                        type="text"
+                        maxLength="2"
+                        size="2"
+                      />
+                    </p>
+                    <p className="control">
+                      <label className="label">Incluir</label>
+                      <Link to="#" className="button is-info">
+                        +
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-11-mobile is-offset-1-mobile">
+                {/* New Feature here */}
+                <div className="field">
+                  <label className="label">Incluídos</label>
+                  <table className="table is-hoverable">
+                    <thead>
+                      <tr>
+                        <th>
+                          <abbr title="Código">Cód.</abbr>
+                        </th>
+                        <th>nome</th>
+                        <th>
+                          <abbr title="Quantidade">Qtde.</abbr>
+                        </th>
+                        <th>Ação</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>42356</td>
+                        <td>Fire TV Stick</td>
+                        <td>1</td>
+                        <td>
+                          <div className="field is-grouped">
+                            <p className="control">
+                              <Link
+                                to="#"
+                                className="button is-danger is-small"
+                              >
+                                -
+                              </Link>
+                            </p>
+                            <p className="control">
+                              <Link
+                                to="#"
+                                className="button is-warning is-small"
+                              >
+                                +
+                              </Link>
+                            </p>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           )}
