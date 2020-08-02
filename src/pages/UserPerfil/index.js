dd5fd9f1d52ed0bdf74d75af1a7026ab4edc6b6c
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2020-08-02 00:22:03 -0300

    Add Perfil page (mock page)

---
+++ src/pages/UserPerfil/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const UserPerfil = () => (
+  <>
+    <Header />
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Perfil de Usuário</h1>
+          <h2 class="subtitle">Configuração e ajustes.</h2>
+          <h3>...em construção</h3>
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+export default UserPerfil;
