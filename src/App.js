06224e7969372b50b645882e5c1872c6dccf8f07
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2022-10-16 20:11:17 -0300

    build!: demolish and rebuild the full project

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Routes from "./config/routes";
-
-import "./App.css";
-// import "bulma/css/bulma.css";
-
-const App = () => <Routes />;
-
-export default App;
--- src/helpers/customer.js
+++
@@ -1,6 +0,0 @@
-import api from "../config/api";
-
-export const getAllNames = () => api.get("/customer").then((list) => list.data);
-
-export const setNewCustomer = async (customer) =>
-  api.post("/customer", customer);
